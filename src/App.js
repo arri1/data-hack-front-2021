4f1040dd399fcbe968581a71eccbb210751f73dc
arri <ewws@mail.ru>
2021-08-12 20:01:24 +0900

    added router lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Login from './pages/login'
+import Main from './pages/main'
+
 const App = () => {
     return (
-        <div>
-            hello
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route
+                    exact
+                    path={'/login'}
+                    component={Login}
+                />
+                <Route
+                    exact
+                    path={'/'}
+                    component={Main}
+                />
+            </Switch>
+        </BrowserRouter>
     );
 }
 
