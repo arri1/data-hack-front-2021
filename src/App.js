dacdfbb9c30315909846657f681333cf6d0a753d
arri <ewws@mail.ru>
2021-08-12 20:49:47 +0900

    added box login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import styled from 'styled-components'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from './pages/login'
 import Main from './pages/main'
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route
-                    exact
-                    path={'/login'}
-                    component={Login}
-                />
-                <Route
-                    exact
-                    path={'/'}
-                    component={Main}
-                />
-            </Switch>
-        </BrowserRouter>
+        <Container>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        exact
+                        path={'/login'}
+                        component={Login}
+                    />
+                    <Route
+                        exact
+                        path={'/'}
+                        component={Main}
+                    />
+                </Switch>
+            </BrowserRouter>
+        </Container>
     );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,26 @@
-const Login = ()=>{
-    return(
-        <div>
-            Login
-        </div>
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex:1;
+`
+
+const Box = styled.div`
+  display: flex;
+  border-radius: 10px;
+  min-height: 200px;
+  min-width: 200px;
+  border: 0.5px solid gray;
+  flex-direction: column;
+`
+
+const Login = () => {
+    return (
+        <Container>
+            <Box/>
+        </Container>
     )
 
 }
